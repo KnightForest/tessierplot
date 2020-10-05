f41a4b8c902f07e583a25fd2d34c5a015a441b85
Joost <joost.ridderbos@unibas.ch>
2020-10-05 17:47:40 +0200

    Fixed syntax error in view.py

--- tessierplot/view.py
+++ tessierplot/view.py
@@ -272,7 +272,7 @@ class tessierView(object):
 
                 <div class='name'> {{ item.measname + '\n' + 'Comment: ' + item.comment }} </div>
 
-                <div class='thumb'>
+                <div class='thumb'>s
                         <img src="{{ item.thumbpath }}?{{ nowstring }}"/> 
                 </div>
 
@@ -300,11 +300,12 @@ class tessierView(object):
                         <option value="{{"\\'meansubtract\\',\\'deinterlace0\\',\\'mov_avg\\',\\'diff\\'"|e}} ">deinterlace0,diff</option>
                         <option value="{{"\\'meansubtract\\',\\'deinterlace1\\',\\'mov_avg\\',\\'diff\\'"|e}} ">deinterlace1,diff</option>
                     </select>
-                    {# <select name="value_axis">
-                        <option value="{{-1|e}}">-1</option>
-                        <option value="{{0|e}}">0</option>
-                        <option value="{{1|e}}">1</option>
-                        <option value="{{2|e}}">2</option>
+                    {#
+                    <select name="value_axis">
+                        <option value="{{-1}}">-1</option>
+                        <option value="{{0}}">0</option>
+                        <option value="{{1}}">1</option>
+                        <option value="{{2}}">2</option>
                     </select> #}
                     <input type="checkbox" name="stylechecker" value="{{"\\'flipaxes\\',"|e}} ">Flip axes
                     </form>            
@@ -383,19 +384,20 @@ class tessierView(object):
                 var style = "["{{" + stylevalues + "|e}}"]";
                 return style
             }
+
             function getValue_axis(id) {
                 id = id.replace(/\\\\/g,"\\\\\\\\");
                 var x = document.querySelectorAll('form[name=\\"'+id+'\\"]')
                 form = x[1]; //should be only one form (not anymore)
                 selector = form.selector;
                 var value_axis = selector.options[selector.selectedIndex].value
-                var value_axis = {{ value_axis }};
-                return value_axis
+                var value_axis = {{value_axis|e}};
+                return axis
             }
             function plotwithStyle(id) {
                 var style = getStyle(id);
-                var value_axis = getValue_axis(id);
-                plot(id,style,value_axis);
+                var value_axis = form.value_axis
+                plot(id,style);
             }
             
             function plot(id,style,value_axis,y){
@@ -487,7 +489,7 @@ class tessierView(object):
         """
         temp = jj.Template(out)
 
-        plotcommand = """\\nimport matplotlib.pyplot as plt\\nimport imp\\nif not plt.get_fignums():\\n from tessierplot import plot as ts\\n imp.reload(ts)\\np = ts.plotR(filename)\\np.quickplot(style=%s)\\n"""
+        plotcommand = """\\nimport matplotlib.pyplot as plt\\nimport imp\\nif not plt.get_fignums():\\n from tessierplot import plot as ts\\n imp.reload(ts)\\np = ts.plotR(filename)\\np.quickplot(style= %s)\\n"""
         
         import datetime
         d=datetime.datetime.utcnow()
