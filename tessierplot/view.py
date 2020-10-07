2471f14daa7a1271967f3c24933999a739b158ed
Joost Ridderbos <joost.ridderbos@unibas.ch>
2020-10-07 16:52:43 +0200

    Fixed game breaking bug in view.py

--- tessierplot/view.py
+++ tessierplot/view.py
@@ -400,11 +400,11 @@ class tessierView(object):
                 plot(id,style);
             }
             
-            function plot(id,style,value_axis,y){
+            function plot(id,x,y){
                 dir = id.split('/');
                 
                 exec = 'filename \= \"' + id + '\"; {{ plotcommand }}';
-                exec = exec.printf(style)
+                exec = exec.printf(x)
 
                 pycommand(exec);
             }
@@ -489,7 +489,7 @@ class tessierView(object):
         """
         temp = jj.Template(out)
 
-        plotcommand = """\\nimport matplotlib.pyplot as plt\\nimport imp\\nif not plt.get_fignums():\\n from tessierplot import plot as ts\\n imp.reload(ts)\\np = ts.plotR(filename)\\np.quickplot(style= %s)\\n"""
+        plotcommand = """\\nimport matplotlib.pyplot as plt\\nimport imp\\nif not plt.get_fignums():\\n from tessierplot import plot as ts\\n imp.reload(ts)\\np = ts.plotR(filename)\\np.quickplot(style=%s)\\n"""
         
         import datetime
         d=datetime.datetime.utcnow()
