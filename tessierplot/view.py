565baa5d9b4bd5d927463380a92a6c9fb03762b7
fuge <fuge>
2020-10-20 10:41:38 +0200

    Added some and renamed some styles for view

--- tessierplot/view.py
+++ tessierplot/view.py
@@ -287,11 +287,13 @@ class tessierView(object):
                         <option value="{{"\\'\\'"|e}}">normal</option>
                         <option value="{{"\\'int\\'"|e}}">int</option>
                         <option value="{{"\\'log\\'"|e}}">log</option>
-                        <option value="{{"\\'mov_avg(n=3)\\',\\'didv\\',\\'mov_avg(n=3)\\'"|e}}">diff</option>
-                        <option value="{{"\\'mov_avg(n=12)\\',\\'didv\\',\\'mov_avg(n=12)\\'"|e}}">diff_smooth</option>
                         <option value="{{"\\'logdb\\'"|e}}">logdb</option>
-                        <option value="{{"\\'sgdidv\\'"|e}}">sgdiff</option>
-                        <option value="{{"\\'sgdidv\\',\\'log\\'"|e}} ">sgdiff,log</option>
+                        <option value="{{"\\'mov_avg(n=12)\\'"|e}}">movavg</option>
+                        <option value="{{"\\'savgol(samples=7,order=3)\\'"|e}}">Savitzky-Golay_filter</option>
+                        <option value="{{"\\'mov_avg(n=3)\\',\\'didv\\',\\'mov_avg(n=3)\\'"|e}}">diff_movavg</option>
+                        <option value="{{"\\'mov_avg(n=12)\\',\\'didv\\',\\'mov_avg(n=12)\\'"|e}}">diff_movavg_smooth</option>
+                        <option value="{{"\\'sgdidv\\'"|e}}">Savitzky-Golay_diff</option>
+                        <option value="{{"\\'sgdidv\\',\\'log\\'"|e}} ">Savitzky-Golay_diff,log</option>
                         <option value="{{"\\'movingmeansubtract(window=1)\\'"|e}} ">movmeansubtract</option>
                         <option value="{{"\\'meansubtract\\',\\'ivreversernew\\',\\'mov_avg(n=3)\\',\\'didv\\'"|e}}">ivreverser,diff</option>
                         <option value="{{"\\'int\\',\\'meansubtract\\',\\'ivreversernew\\',\\'didv\\'"|e}}">int,ivreverser,diff</option>
