Fixed horizontal colorbar scaling, minor fixes in styles and colorbar

tessierplot/plot.py
@@ -21,10 +21,10 @@ else:
 import IPython #Importing IPython modules
 ipy=IPython.get_ipython()
 if isqt5:
-	ipy.magic("pylab qt5")
+	ipy.run_line_magic('matplotlib', 'qt5')
 	qtaggregator = 'Qt5Agg'
 elif isqt4:
-	ipy.magic("pylab qt")
+	ipy.run_line_magic('matplotlib', 'qt')
 	qtaggregator = 'Qt4Agg'
 else:
 	print('no backend found.')
@@ -361,24 +361,6 @@ class plotR(object):
 				ext = (x.min(),x.max(),y.min(),y.max())
 				self.extent = ext
 				
-				#setting custom xlims and ylims, restricted within extent of data
-				if xlims == None:
-					_xlims = (ext[0],ext[1])
-				else: #sets custum xlims only if they restrict the default xaxis
-					xzip = list(zip(xlims,[ext[0],ext[1]]))
-					xlimsl = 2*[None]
-					xlimsl[0]=max(xzip[0])
-					xlimsl[1]=min(xzip[1])
-					_xlims=tuple(xlimsl)
-
-				if ylims == None:
-					_ylims = (ext[2],ext[3])
-				else: #sets custom xyims only if they restrict the default yaxis
-					yzip = list(zip(ylims,[ext[2],ext[3]]))
-					ylimsl = 2*[None]
-					ylimsl[0]=max(yzip[0])
-					ylimsl[1]=min(yzip[1])
-					_ylims=tuple(ylimsl)
 
 				#Gridding and interpolating unevenly spaced data
 				extx = abs(ext[1]-ext[0])
@@ -422,21 +404,7 @@ class plotR(object):
 				self.X = X
 				self.Y = Y
 				self.exportData.append(XX)
-				try:
-					m={
-						'xu':xu,
-						'yu':yu,
-						'xlims':_xlims,
-						'ylims':_ylims,
-						'zlims':(0,0),
-						'xname':coord_keys[-2],
-						'yname':coord_keys[-1],
-						'zname':'unused',
-						'datasetname':self.name}
-					self.exportDataMeta = np.append(self.exportDataMeta,m)
-				except Exception as e:
-					print(e)
-					pass
+
 				if ax_destination is None:
 					ax = plt.subplot(gs[cnt])
 				else:
@@ -546,6 +514,40 @@ class plotR(object):
 					self.imshow_norm = w['imshow_norm']
 				if norm == 'nan':
 					self.imshow_norm = None
+				#setting custom xlims and ylims, restricted within extent of data
+				if xlims == None:
+					_xlims = (ext[0],ext[1])
+				else: #sets custum xlims only if they restrict the default xaxis
+					xzip = list(zip(xlims,[ext[0],ext[1]]))
+					xlimsl = 2*[None]
+					xlimsl[0]=max(xzip[0])
+					xlimsl[1]=min(xzip[1])
+					_xlims=tuple(xlimsl)
+
+				if ylims == None:
+					_ylims = (ext[2],ext[3])
+				else: #sets custom xyims only if they restrict the default yaxis
+					yzip = list(zip(ylims,[ext[2],ext[3]]))
+					ylimsl = 2*[None]
+					ylimsl[0]=max(yzip[0])
+					ylimsl[1]=min(yzip[1])
+					_ylims=tuple(ylimsl)
+
+				try:
+					m={
+						'xu':xu,
+						'yu':yu,
+						'xlims':_xlims,
+						'ylims':_ylims,
+						'zlims':(0,0),
+						'xname':coord_keys[-2],
+						'yname':coord_keys[-1],
+						'zname':'unused',
+						'datasetname':self.name}
+					self.exportDataMeta = np.append(self.exportDataMeta,m)
+				except Exception as e:
+					print(e)
+					pass
 
 				# This deinterlace needs to be reworked. There are no colorbars for instance..
 				if 'deinterlace' in style:
@@ -632,11 +634,11 @@ class plotR(object):
 						divider = make_axes_locatable(ax)
 						if cbar_orientation == 'horizontal': # Added some hardcode config for colorbar, more pretty out of the box
 							cax = divider.append_axes("top", size="5%", pad=0.05)
-							cax.set_aspect(0.07)
+							cax.set_box_aspect(0.07)
 							cax.set_anchor('E')
 						else:
 							cax = divider.append_axes("right", size="2.5%", pad=0.05)
-						pos = list(ax.get_position().bounds)
+						#pos = list(ax.get_position().bounds)
 					if hasattr(self, 'im'):
 						self.cbar = colorbar.create_colorbar(cax, self.im, orientation=cbar_orientation)
 						cbar = self.cbar