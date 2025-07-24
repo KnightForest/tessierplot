6ef0e690b0ba87cc69cf913b3ef4fb93a7e9c0e4
Ridderbos <j.ridderbos@utwente.nl>
2025-07-24 12:28:09 +0200

    Migrated to modern install using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='tessierplot',
-      version='0.2',
-      description='Module for plotting/manipulating 2d/3d data',
-      url='http://github.com/wakass/tessierplot',
-      author='WakA',
-      author_email='alaeca@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=[
-          'matplotlib',
-          'pyperclip',
-          'six',
-          'pandas',
-          'pywin32',
-          'quantiphy',
-          'scipy',
-          'peakutils',
-          'numpy',
-          'pyqt5',
-          'notebook==6.5.6'
-      ],
-      zip_safe=False)
+setup()
